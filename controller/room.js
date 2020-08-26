c35cf0f9f79b4acfae6aaf63d09565887f1941b1
iwagithub <iwakami-tomoya-ch@ynu.jp>
2020-08-26 14:22:40 +0900

    整理

--- controller/room.js
+++ controller/room.js
@@ -1,20 +1,8 @@
 var app = require('express')();
-var server = require('http').Server(app);
-var user = require("../model/user");
-// var io = require("./io");
 
 // http://localhost:8124/test2/?id=121
 app.get('/', function (req, res) {
-    // res.sendFile(__dirname + '/test.html');
-    console.log(req.query.id);
-    // user = new User(req.query.id);
-    // console.log(user);
-    // console.log(user.userID);
-    // res.send(`test2 page. userID:` + user.getInfo());
-    var myUser = new user.User(1);
-    console.log(myUser);
-    console.log(myUser.userId);
-    // io(server);
+    // console.log(req.query.id);
     res.sendFile(__dirname + "/index.html");
 });
 
