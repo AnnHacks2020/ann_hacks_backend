83afbf9f5ddbc01e1f44fb087785bc6277fab279
iwagithub <iwakami-tomoya-ch@ynu.jp>
2020-08-27 11:11:03 +0900

    room.update追加

--- controller/io.js
+++ controller/io.js
@@ -31,6 +31,7 @@ function io(server) {
         socket.on('server send', function (msg) {
             // 自分以外の全員に送る
             // io.to(store[msg.id].room).emit('send user', msg); //こちらだと自分にも送られる
+            room.update(mgs.roomid, msg.id, "base64image...", "99");
             socket.broadcast.to(store[msg.id].room).emit('send user', msg);
         });
     
